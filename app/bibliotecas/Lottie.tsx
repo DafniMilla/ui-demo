04a71a05eb5ca320195601b53e685545d2eb54f1
DafniMilla <dafni8663@gmail.com>
2025-11-11 15:47:09 -0300

    lottie OK

--- app/bibliotecas/Lottie.tsx
+++ app/bibliotecas/Lottie.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
+import LottieView from 'lottie-react-native';
 
 export default function NativePaper() {
   const router = useRouter();
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>React Native Paper</Text>
+
+      <LottieView
+      style={{width:100, height:100}}
+        source={require('../../assets/images/animacao.json')}
+          autoPlay
+          />
+      
+
       <Button title="Voltar" onPress={() => router.back()} />
     </View>
   );
