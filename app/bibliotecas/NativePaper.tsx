f3b1cbbe23bf54fb254c2b26ddfa01fcc12ed359
karlos <karlosmarques@gmail.com>
2025-11-13 08:29:30 -0300

    login com NativePaper

--- app/bibliotecas/NativePaper.tsx
+++ app/bibliotecas/NativePaper.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { router } from 'expo-router';
+import * as React from 'react';
+import { View } from 'react-native';
+import { Appbar, Button, Text, TextInput } from 'react-native-paper';
 
-export default function NativePaper() {
-  const router = useRouter();
+const MyComponent = () => (
+  <>
+    <Appbar.Header>
+      <Appbar.BackAction onPress={() => router.back()} />
+      <Appbar.Content title="Voltar" />
+    </Appbar.Header>
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>React Native Paper</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+      }}
+    >
+      <Text style={{ textAlign: 'center', fontSize: 24, marginBottom: 24 }}>
+        Login
+      </Text>
+
+      <TextInput
+        label="Email"
+        mode="outlined"
+        left={<TextInput.Icon icon="at" color="black" />}
+        style={{ width: '85%', marginBottom: 16 }}
+      />
+
+      <TextInput
+        label="Senha"
+        mode="outlined"
+        secureTextEntry
+        left={<TextInput.Icon icon="lock" color="black" />}
+        style={{ width: '85%', marginBottom: 24 }}
+      />
+
+      <Button
+        mode="contained"
+        style={{ width: '85%', borderRadius: 8 }}
+        onPress={() => console.log('Entrar')}
+      >
+        Entrar
+      </Button>
     </View>
-  );
-}
+  </>
+);
+
+export default MyComponent;
